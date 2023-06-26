d217553769800a66747b969b47fa07885bdcdffc
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-06-26 16:54:24 +0000

    Add structure and fix loop

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import random
+from art import body_pieces
 
 word_list = [ 
     'elephant', 'guitar', 'computer', 'pizza', 'bicycle', 'diamond', 'rainbow',
-    'spider', 'telescope', 'dragon', 'house', 'book', 'bed', 'Summer', 'school'
+    'spider', 'telescope', 'dragon', 'house', 'book', 'bed', 'summer', 'school',
+    'computer', 'park', 'office', 'bodybuilder', 
 ]
 
 set_word = random.choice(word_list)
 # test words
 print(f'Test: {set_word}')
 
+tries = 6 # The artwork is divided into 6 parts 
+game_over = False
+
 board = []
 for letter in set_word:
     board += "_"
 
-game_over = False
-
 while not game_over: 
     player_chose = input("Write a letter here: ").lower()
-
+    
     for position_letter in range(len(set_word)):
         letter = set_word[position_letter]
         if letter == player_chose:
             board[position_letter] = letter
-    print(board)
-
+            tries -= 1
+            if tries == 0:
+                game_over = True
+                print("You've lost your head!")
+        
     if "_" not in board:
         set_word = True
         print("Congratulations, you have survived!")
-        
