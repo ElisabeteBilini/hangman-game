6109e0c65df4fca18175854bf35d3f4b6cf930eb
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-06-26 09:56:54 +0000

    Fix game structure and loop

--- index.py
+++ index.py
@@ -8,14 +8,19 @@ word_list = [
 set_word = random.choice(word_list)
 print(f'Test: {set_word}')
 
-player_chose = input("Write a letter here: ").lower()
+while set_word: 
+    player_chose = input("Write a letter here: ").lower()
 
-board = []
-for letter in set_word:
-    board += "_"
+    board = []
+    for letter in set_word:
+        board += "_"
 
-for position_letter in range(len(set_word)):
-    letter = set_word[position_letter]
-    if letter == player_chose:
-        board[position_letter] = letter
-print(board)
+    for position_letter in range(len(set_word)):
+        letter = set_word[position_letter]
+        if letter == player_chose:
+            board[position_letter] = letter
+    print(board)
+
+if "_" not in board:
+    set_word = True
+    print("You win!")
