6921e17ea1e452c2ebcf56b9c28b7a0a46911866
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-06-28 16:11:42 +0000

    Fix loop and change the structure

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ word_list = [
     'spider', 'telescope', 'dragon', 'house', 'book', 'bed', 'summer', 'school',
     'computer', 'park', 'office', 'bodybuilder' 
 ]
+
 #choosing random word
 select_word = random.choice(word_list).upper()
 print(select_word)
@@ -12,15 +13,19 @@ print(select_word)
 #request letter to player
 letter_player = input("Type one letter: ").upper()
 
-#Checking Letter
-for letter in select_word:
+#Checking Letter and spaces
+board = []
+chances = 6
+
+for letter in range(len(select_word)):
+    board += '_'
+
+for letter_position in range(len(select_word)):
+    letter = select_word[letter_position]
     if letter == letter_player:
-        print("you're right!")
-    else:
-        print("wrong!")
+        board[letter_position] = letter
 
-
-
+print(board)
 
 
 
