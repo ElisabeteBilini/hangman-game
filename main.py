b69362c16db87394232587b653f675662b161627
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-06-28 09:22:56 +0000

    Fix structure

--- main.py
+++ main.py
@@ -5,31 +5,54 @@ word_list = [
     'spider', 'telescope', 'dragon', 'house', 'book', 'bed', 'summer', 'school',
     'computer', 'park', 'office', 'bodybuilder' 
 ]
+#choosing random word
+select_word = random.choice(word_list).upper()
+print(select_word)
 
-set_word = random.choice(word_list)
-# test words
-print(f'Test: {set_word}')
+#request letter to player
+letter_player = input("Type one letter: ").upper()
 
-tries = 6 # The artwork is divided into 6 parts 
-game_over = False
+#Checking Letter
+for letter in select_word:
+    if letter == letter_player:
+        print("you're right!")
+    else:
+        print("wrong!")
 
-board = []
-for letter in set_word:
-    board += "_"
 
-while not game_over: 
-    player_chose = input("Write a letter here: ").lower()
-    
-    for position_letter in range(len(set_word)):
-        letter = set_word[position_letter]
-        if letter == player_chose:
-            board[position_letter] = letter
-    if player_chose not in set_word:
-        tries -= 1
-        if tries == 0:
-            game_over = True
-            print("You've lost your head!")
-                    
-    if "_" not in board:
-        game_over = True
-        print("Congratulations, you have survived!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
