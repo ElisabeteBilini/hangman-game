3a8bfba6cb9a085e698a02c01bbb7835f7b99756
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-06-28 18:13:43 +0000

    Fix structure and change art

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from art import body_pieces, hang, you
 import random
+
+print(hang)
 
 word_list = [ 
     'elephant', 'guitar', 'computer', 'pizza', 'bicycle', 'diamond', 'rainbow',
@@ -6,14 +9,15 @@ word_list = [
     'computer', 'park', 'office', 'bodybuilder' 
 ]
 
+#choosing random word
+select_word = random.choice(word_list).upper()
+print(select_word)
+
+
 chances = 6 # body has 6 pieces
 board = []
 blanks = '_'
 game_over = False
-
-#choosing random word
-select_word = random.choice(word_list).upper()
-print(select_word)
 
 for letter in range(len(select_word)):
         board += blanks
@@ -27,15 +31,18 @@ while not game_over:
         letter = select_word[letter_position]
         if letter == letter_player:
             board[letter_position] = letter
-        else:
-            print("#")
-
     print(board)
+
+    if letter_player not in select_word:
+        chances -= 1
+        if chances == 0:
+            game_over = True
+            print("Beware, words can also kill! You lose.")
 
     if blanks not in board:
         game_over = True
+        print(you)
         print("Congratulation, you survive!")
-
 
 
 
