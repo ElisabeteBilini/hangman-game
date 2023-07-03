e15c0d6807262356daa72aea427006ba88a2f61e
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-07-03 08:03:22 +0000

    Fix structure and add new player

--- main.py
+++ main.py
@@ -21,20 +21,26 @@ game_over = False
 for letter in range(len(select_word)):
         board += blanks
 
-player_01 = input("Player 1, enter your name: ")
-player_02 = input("Player 1, enter your name: ")
+player_01 = input("Player 1, enter your name: ").upper()
+player_02 = input("Player 2, enter your name: ").upper()
 
-current_player = player_01
+atual_player = player_01
 
 while not game_over:
+    #identifying player
+    print('Player :' + ''.join(board))
+    print(f"{atual_player}'s turn.")
+
     #request letter to player
     letter_player = input("Type one letter: ").upper()
 
     #Checking Letter and spaces
+    has_letter = False
     for letter_position in range(len(select_word)):
         letter = select_word[letter_position]
         if letter == letter_player:
             board[letter_position] = letter
+            has_letter = True
     print(board)
 
     if letter_player not in range(len(select_word)):
