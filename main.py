e43c492bb8135e449e28d87544610eeecdfe5198
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-07-03 09:48:05 +0000

    Fix structure and fix loop players

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from art import body_pieces, hang, you
+from art import body_pieces, hang
 import random
 
 print(hang)
@@ -20,47 +20,82 @@ game_over = False
 
 for letter in range(len(select_word)):
         board += blanks
-        
-player_01 = input("Player 1, enter your name: ").upper()
-player_02 = input("Player 2, enter your name: ").upper()
 
-atual_player = player_01
+choice = input("Choose the game mode: 1 Single Player or 2 Two Players. ")
 
-while not game_over:
-    #identifying player
-    print('Player :' + ''.join(board))
-    print(f"{atual_player}'s turn.")
+print("Number of letters in the secret word: ", len(select_word))
 
-    #request letter to player
-    letter_player = input("Type one letter: ").upper()
+if choice == '1':
+    player = input("Player 1, enter your name: ").upper()
 
-    #Checking Letter and spaces
-    has_letter = False
-    for letter_position in range(len(select_word)):
-        letter = select_word[letter_position]
-        if letter == letter_player:
-            board[letter_position] = letter
-            has_letter = True
-
-    if not has_letter:
-        chances -= 1
-        if chances == 0:
-            game_over = True
-            print("Beware, words can also kill! You lose.")
-            print(f'{atual_player} lost the game.')
-            break
-
-    if blanks not in board:
-        game_over = True
-        print("Congratulation, you survive!")
-        print(f'{atual_player} won the game.')
+    while not game_over:
+        #identifying player
+        print('Player :' + ''.join(board))
     
-    if atual_player == player_01:
-        atual_player = player_02
-    else:
-        atual_player = player_01
-    print(body_pieces[chances])
+        #request letter to player
+        letter_player = input("Type one letter: ").upper()
 
+        #Checking Letter and spaces
+        has_letter = False
+        for letter_position in range(len(select_word)):
+            letter = select_word[letter_position]
+            if letter == letter_player:
+                board[letter_position] = letter
+                has_letter = True
+
+        if not has_letter:
+            chances -= 1
+            if chances == 0:
+                game_over = True
+                print("Beware, words can also kill! You lose.")
+                print(f'{atual_player} lost the game.')
+                break
+            print(body_pieces[chances])
+
+        if blanks not in board:
+            game_over = True
+            print("Congratulation, you survive!")
+        
+else:       
+    player_01 = input("Player 1, enter your name: ").upper()
+    player_02 = input("Player 2, enter your name: ").upper()
+
+    atual_player = player_01
+
+    while not game_over:
+        #identifying player
+        print('Player :' + ''.join(board))
+        print(f"{atual_player}'s turn.")
+
+        #request letter to player
+        letter_player = input("Type one letter: ").upper()
+
+        #Checking Letter and spaces
+        has_letter = False
+        for letter_position in range(len(select_word)):
+            letter = select_word[letter_position]
+            if letter == letter_player:
+                board[letter_position] = letter
+                has_letter = True
+
+        if not has_letter:
+            chances -= 1
+            if chances == 0:
+                game_over = True
+                print("Beware, words can also kill! You lose.")
+                print(f'{atual_player} lost the game.')
+                break
+
+        if blanks not in board:
+            game_over = True
+            print("Congratulation, you survive!")
+            print(f'{atual_player} won the game.')
+        
+        if atual_player == player_01:
+            atual_player = player_02
+        else:
+            atual_player = player_01
+        print(body_pieces[chances])
 
 
 
